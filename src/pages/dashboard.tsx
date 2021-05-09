bdf95c9b4ec35bc7ea19c8969b320e34636422bd
Enos Domingues <enosdomingues@gmail.com>
2021-05-08 21:10:30 -0300

    Broadcast de logout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@chakra-ui/button"
 import React, { useContext, useEffect } from "react"
 import { withSSRAuth } from "../../utils/withSSRAuth"
 import { Can } from "../components/Can"
@@ -6,15 +7,17 @@ import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 
 export default function Dashboard() {
-  const { user, isAuthenticated } = useContext(AuthContext)
+  const { user, signOut, isAuthenticated } = useContext(AuthContext)
 
   useEffect(() => {
-    api.get('/me').then(response => console.log(response))
+    api.get('/me')
   }, [])
 
   return (
     <>
      <h1>Dashboard: {user?.email}</h1>
+
+     <Button onClick={() => signOut()} colorScheme="blue">Sign out</Button>
 
      <Can permissions={['metrics.list']}>
         <div>Métricas</div>
@@ -26,7 +29,6 @@ export default function Dashboard() {
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx)
   const response = await apiClient.get('/me')
-  console.log(response)
 
   return {
     props: {}
